07ccdae414828ec35a308cdd1e15a6b18c2099b5
thomp <thomp@thomp-Inspiron-5748>
2015-05-06 14:49:19 -0500

    AEResponse::getResponseDate update

--- yii/protected/models/AeResponse.php
+++ yii/protected/models/AeResponse.php
@@ -206,18 +206,34 @@ class AeResponse extends CActiveRecord
 		return $json_responses;		
 	}
 	
-	public static function getResponseDate($duration,$from_date,$user_id)
+	public static function getResponseDate($dateObj/*$duration,$from_date*/,$user_id)
 	{
-		if (!$duration) $duration = 30;
-		if (!$from_date) $from_date = date('Y-m-d');
-		$list = Yii::app()->db->createCommand(
-			"select distinct(DATE(date_created)) as dates from ae_response 
-			where date_created>=date_sub(date('".$from_date."'),interval :duration day)
-			and DATE(date_created)<=date('".$from_date."')
-			and user_id = :user_id
-			order by(DATE(date_created)) DESC")->bindValues(
-			array(':duration'=>$duration,':user_id'=>$user_id))->queryAll();
-		
+        $from_date = null;
+        $duration = null;
+        if (array_key_exists('from_date',$dateObj)) {
+        	if (array_key_exists('duration',$dateObj)) $duration = $dateObj['duration'];
+        	else $duration = 30;
+        	$from_date = $dateObj['from_date'];
+        } else {
+        	$duration = 30;
+        }
+		//if (!$from_date) $from_date = date('Y-m-d');		
+		if ($from_date) {
+			$list = Yii::app()->db->createCommand(
+				"select distinct(DATE(date_created)) as dates from ae_response 
+				where date_created>=date_sub(date('".$from_date."'),interval :duration day)
+				and DATE(date_created)<=date('".$from_date."')
+				and user_id = :user_id
+				order by(DATE(date_created)) DESC")->bindValues(
+				array(':duration'=>$duration,':user_id'=>$user_id))->queryAll();
+		} else {
+			$from_date = date('Y-m-d');
+			$list = Yii::app()->db->createCommand(
+					"select distinct(DATE(date_created)) as dates from ae_response
+					where user_id= :user_id
+					order by(DATE(date_created)) DESC limit :duration")->bindValues(
+					array(':duration'=>$duration,':user_id'=>$user_id))->queryAll();
+		}
 		$dates = array();
 		
 		foreach ($list as $d) {array_push($dates,$d['dates']);}
