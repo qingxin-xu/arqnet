6fed7f86890a7411c4dd0398467b3a25af2c4a2d
thomp <thomp@thomp-Inspiron-5748>
2015-11-05 13:49:38 -0600

    - Ethnicity data

---
+++ yii/protected/models/insert_ethnicity.sql
@@ -0,0 +1,5 @@
+insert into ethnicity(description) values('African American');
+insert into ethnicity(description) values('Asian');
+insert into ethnicity(description) values('Caucasian');
+insert into ethnicity(description) values('Latino');
+insert into ethnicity(description) values('Native American');
